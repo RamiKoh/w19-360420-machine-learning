2e86f466ae12a4dc6db433ffe901eab382bce65d
RamiKoh <49964408+RamiKoh@users.noreply.github.com>
2019-05-14 22:00:09 -0400

    Update KNNClassifier.java

--- KNNClassifier.java
+++ KNNClassifier.java
@@ -73,7 +73,7 @@ public class kNNMain
 		  
 		  if (predicted.equals(first))
 		  {
-			  norman++; //True Positives
+			  norman++;
 			  if (predicted.equals("malignant"))
 			  {
 				markzucchini++;
